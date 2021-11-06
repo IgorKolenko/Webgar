1962eaf7bd578f528b589be922e000e09d8c53d8
IgorKolenko <ik52425@fer.hr>
2021-11-06 17:27:51 +0100

    Maknut jsTestcase iz importa

--- client/src/components/noviHtmlZad/noviHtmlZad.js
+++ client/src/components/noviHtmlZad/noviHtmlZad.js
@@ -4,7 +4,6 @@ import '../formElements.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import {faPen} from '@fortawesome/free-solid-svg-icons';
-import JsTestcase from '../jsTestcase/jsTestcase';
 
 class NoviHtmlZad extends React.Component{
     //konstruktor za NoviJsZad
