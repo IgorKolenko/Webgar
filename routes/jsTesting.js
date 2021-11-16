4786cf9d555f71dc0dceaddbdff484ec9cc14de6
Piro <spiro.golem>
2021-11-16 19:16:01 +0100

    Dodana funkcionalnost za file

--- routes/jsTesting.js
+++ routes/jsTesting.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
+const fs = require('fs');
 
 var evaluator=require('eval');
 const {raw} = require("express");
@@ -9,41 +10,77 @@ router.get('/jsTesting',function (req,res,next){
     // Mock testcase
     let testcase = {
         imeFunkcije: "f1",
-        input:"a,b",
-        output: "ab"
+        input:"1,2",
+        output: "3"
     };
-    // Mock function
-    let fjaTest="function f1(p1,p2){\n" +
-        "p3=p1+p2;\n" +
-        "return p3;\n" +
-        "}"
 
     //TODO Check incorrect function name
 
-    // Check input for Numbers
-    var rawInput=testcase.input.split(',')
-    rawInput.forEach(function(element,i){
-        if(!isNaN(element)){
-            rawInput[i]=Number(element)
+
+    // Check file
+    fs.readFile('../ProjektR/routes/test.js','utf8',(err,data)=>{
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+        let fjaTest=data
+        // Check input for Numbers
+        var rawInput=testcase.input.split(',')
+        rawInput.forEach(function(element,i){
+            if(!isNaN(element)){
+                rawInput[i]=Number(element)
+            }
+        })
+
+        // Find function declaration
+        let fIndex=fjaTest.search("function "+testcase.imeFunkcije);
+        // Add module.exports to start
+        let formattedFunction=fjaTest.slice(0,fIndex)+" module.exports = "+fjaTest.slice(fIndex)
+
+
+        // Get Function result
+        var result=evaluator(formattedFunction)(...rawInput)
+        console.log(result)
+
+        if (result==testcase.output){
+            console.log("SUCCESS")
+        }else{
+            console.log("FAIL")
         }
     })
 
-    // Find function declaration
-    let fIndex=fjaTest.search("function "+testcase.imeFunkcije);
-    // Add module.exports to start
-    let formattedFunction=fjaTest.slice(0,fIndex)+" module.exports = "+fjaTest.slice(fIndex)
 
+    // Check String
+    // Mock function
+    // let fjaTest="function f1(p1,p2){\n" +
+    //     "p3=p1+p2;\n" +
+    //     "return p3;\n" +
+    //     "}"
 
-    // Get Function result
-    var result=evaluator(formattedFunction)(...rawInput)
-    console.log(result)
-
-    if (result==testcase.output){
-        console.log("SUCCESS")
-    }else{
-        console.log("FAIL")
-    }
-
+    // // Check input for Numbers
+    // var rawInput=testcase.input.split(',')
+    // rawInput.forEach(function(element,i){
+    //     if(!isNaN(element)){
+    //         rawInput[i]=Number(element)
+    //     }
+    // })
+    //
+    // // Find function declaration
+    // let fIndex=fjaTest.search("function "+testcase.imeFunkcije);
+    // // Add module.exports to start
+    // let formattedFunction=fjaTest.slice(0,fIndex)+" module.exports = "+fjaTest.slice(fIndex)
+    //
+    //
+    // // Get Function result
+    // var result=evaluator(formattedFunction)(...rawInput)
+    // console.log(result)
+    //
+    // if (result==testcase.output){
+    //     console.log("SUCCESS")
+    // }else{
+    //     console.log("FAIL")
+    // }
 
     res.json({ok:true});
 });
