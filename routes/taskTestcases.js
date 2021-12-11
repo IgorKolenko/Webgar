9b846b0305a822777df8d6405e71538a5172c2b2
Piro <spiro.golem>
2021-12-11 17:47:01 +0100

    Slight changes

--- routes/taskTestcases.js
+++ routes/taskTestcases.js
@@ -9,20 +9,24 @@ router.post('/addTask',async function (req,res,next){
     const{imeZadatak,opisZadatak,vrstaZadatak, testcases}=req.body
 
     // Insert Task into database
-    res=await db.query('INSERT INTO zadatak (imeZadatak,opisZadatak,idProfesor,idVrsta) VALUES($1,$2,1,$3) RETURNING idzadatak', [imeZadatak, opisZadatak, vrstaZadatak])
+    //TODO Currently Hardcoded profesorID
+    result=await db.query('INSERT INTO zadatak (imeZadatak,opisZadatak,idProfesor,idVrsta) VALUES($1,$2,1,$3) RETURNING idzadatak',
+        [imeZadatak, opisZadatak, vrstaZadatak])
 
     // Insert testcase into database
-    console.log("res: "+JSON.stringify(res));
-    console.log("TaksID: "+JSON.stringify(res.rows[0].idzadatak));
+    console.log("res: "+JSON.stringify(result));
+    console.log("TasksID: "+JSON.stringify(result.rows[0].idzadatak));
     console.log("Testcases: "+JSON.stringify(testcases));
-    testcases.forEach(async (testcase) => {
-        // console.log(JSON.parse(testcase))
+    for (const testcase of testcases) {
         //Body ima vrstaTestCase,imeTestCase,imeFunkcije,input,output
         console.log("Spremanje u bazu testcase: "+testcase.imeTestCase);
         //let testCaseJSON=JSON.stringify({funcName: testcase.JSON.imeFunkcije, input: testcase.JSON.input, output: testcase.JSON.output})
         await db.query(`INSERT INTO testcase (imeTestCase,JSON,vrstaTestCase,idZadatak)
-                            VALUES ('${testcase.imeTestCase}','${JSON.stringify(testcase.JSON)}','${testcase.vrstaTestCase}',${res.rows[0].idzadatak})`);
-    })
+                            VALUES ('${testcase.imeTestCase}','${JSON.stringify(testcase.JSON)}','${testcase.vrstaTestCase}',${result.rows[0].idzadatak})`).catch(
+                                err=>{console.log(err)}
+        );
+    }
+    res.json({ok:true})
 });
 
 module.exports = router
