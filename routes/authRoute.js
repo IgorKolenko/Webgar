7bc6e5ef7381e9e0dcbf440288ca8cc125295715
Piro <spiro.golem>
2021-12-30 14:26:45 +0100

    added authRoute

---
+++ routes/authRoute.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../db');
+const User = require('../models/UserModel')
+const {
+    check,
+    body,
+    validationResult
+} = require('express-validator');
+
+router.post('/login', [body('email').not().isEmpty().isEmail(),
+    body('password').not().isEmpty().isLength({
+        min: 5
+    })]
+    ,async function(req,res,next){
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        console.log(errors)
+        res.sendStatus(500)
+    }else{
+        var email=req.body.email
+        var password=req.body.password
+
+        let user=await User.getByEmail(email)
+        if(user.verifyPassword(password)){
+            console.log(user)
+            req.session.user=user
+            res.sendStatus(200)
+        }else{
+            res.sendStatus(401)
+        }
+
+    }
+})
+
+
+module.exports=router
